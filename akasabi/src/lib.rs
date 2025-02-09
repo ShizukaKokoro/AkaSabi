5c1a06043074d6d0e127842bef70c7791475a4c4
ShizukaKokoro <s1f102300032@toyo.jp>
2025-02-09 18:48:29 +0900

    feat: :tada: 赤錆

---
+++ akasabi/src/lib.rs
@@ -0,0 +1,11 @@
+//! 赤錆
+
+#![warn(
+    missing_docs,
+    rustdoc::missing_crate_level_docs,
+    unused_results,
+    clippy::complexity,
+    clippy::perf,
+    clippy::style
+)]
+#![deny(clippy::correctness, clippy::suspicious)]
