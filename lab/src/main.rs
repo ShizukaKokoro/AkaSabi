7111e78b38d764845b43c12e9714971b1c3e30e3
ShizukaKokoro <s1f102300032@toyo.jp>
2025-02-15 10:31:12 +0900

    build: 開発環境を整える

--- lab/src/main.rs
+++ lab/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    log::trace!("Trace, world!");
+    log::debug!("Debug, world!");
+    log::info!("Hello, world!");
+    log::warn!("Warning, world!");
+    log::error!("Error, world!");
 }
